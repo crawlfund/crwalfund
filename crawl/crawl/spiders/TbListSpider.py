02c30a41625b270b3c17500cfa9f9cceed55897d
luyang00 <393780933@qq.com>
2015-10-25 16:39:24 +0800

    test load json

---
+++ crawl/crawl/spiders/TbListSpider.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import scrapy
+import sys;
+from crawl.items import CrawlFundItem
+import json;
+
+class TbListSpider(scrapy.Spider):
+    name = "tblistspider"
+    allowed_domains = ["taobao.com"]
+    start_urls = (
+        'https://izhongchou.taobao.com/dream/ajax/getProjectList.htm?page=1&pageSize=10000&projectType=&type=6&status=3&sort=5',    )
+
+    def parse(self, response):
+        list=json.loads(response.body_as_unicode())
+        print list
+
+        
