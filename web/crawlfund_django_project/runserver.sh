8ef2e8e0b0dca654495df553dd52aa8cf9f0a9ba
luyang00 <393780933@qq.com>
2015-10-31 09:54:30 +0800

    use flask remove django and create a band with database & html

--- web/crawlfund_django_project/runserver.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-cd ~/Code/crwalfund/web/crawlfund_django_project
-python manage.py runserver 0.0.0.0:1111
--- web/website/runserver.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /home/crwalfund/web/website
-python manage.py runserver 0.0.0.0:1111
