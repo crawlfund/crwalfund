b209ed4b2e220eaa09ec9fa26a392506fb93b9ae
luyang00 <393780933@qq.com>
2015-11-05 00:22:25 +0800

    add detail page ajax valid

--- web/crawlfund_flask_project/index.py
+++ web/crawlfund_flask_project/index.py
@@ -3,7 +3,7 @@
 from flask.ext.pymongo import PyMongo
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
-
+import json
 
 # configuration
 
@@ -26,8 +26,28 @@ def before_request():
 
 @app.route('/')
 def show_entries():
+    a = mongo.db.tblist_items.distinct("id")
     project = mongo.db.tblist_items.find()
     return render_template('index.html',entries=project)
+
+@app.route('/detail/')
+def show_detail():
+    a = mongo.db.tblist_items.distinct("id")
+    project = mongo.db.tblist_items.find()
+    return render_template('detail.html',entries=project)
+
+@app.route('/ajax/request', methods=['GET'])
+def detail2json():
+    if request.method == 'GET':
+        id = int(request.args.get('id', 10))
+        #tag = int(request.args.get('tag'),0)
+        #count = int(request.args.get('count'),0)
+        results = mongo.db['tblist_items'].distinct("id")#.limit(count)
+        json_results= []
+        for result in results:
+            json_results.append(result)
+        print results
+        return json.dumps(json_results)
 
 @app.teardown_request
 def teardown_request(exception):
