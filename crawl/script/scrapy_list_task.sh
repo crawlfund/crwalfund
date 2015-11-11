b318660ce3f6e701aca1fd80f9ebdc39cacbbc81
luyang00 <393780933@qq.com>
2015-11-12 00:09:05 +0800

    add script to server

---
+++ crawl/script/scrapy_list_task.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/wwwroot/crawl.wellmakers.com/crwalfund/crawl/crawl/spiders
+scrapy runspider TbListSpider.py
--- web/crawlfund_flask_project/index.py
+++ web/crawlfund_flask_project/index.py
@@ -62,4 +62,4 @@ def teardown_request(exception):
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0',port=9091)
